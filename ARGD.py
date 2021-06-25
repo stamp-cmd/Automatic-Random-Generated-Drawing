32d86e7931a73171cc032fa776af163a3184525f
Kantanat-Stamp <76716475+stamp-cmd@users.noreply.github.com>
2021-06-25 15:11:31 +0700

    Change Code System
    Add more configuration
    Fix Logo Address Error

--- ARGD.py
+++ ARGD.py
@@ -4,26 +4,37 @@ import turtle
 import random
 import time
 
-root = tkinter.Tk()
-root.iconbitmap("ARGD\ARGD-logo-_2_.ico")
-root.title("Automatic Random Generated Drawing")
-root.resizable(False, False)
-root.configure(bg="#c9c9c9")
+Header = "System 15" #Edit Header font Here
+Paragraph = "System 10" #Edit Paragraph font Here
+Title_Text = "Automatic Random Generated Drawing" #Edit Title Here
+Background_Color = "#c9c9c9" #Edit Bakcground color Here (Hex Code)
+Icon_Address = "ARGD-logo-_2_.ico"
 
-Header = "System 15"
-Paragraph = "System 10"
+
+root = tkinter.Tk()
+root.iconbitmap(Icon_Address)
+root.title(Title_Text)
+root.resizable(False, False)
+root.configure(bg=Background_Color)
 
 Canvas = tkinter.Canvas(root, width=500, height=500, bg="white")
 Canvas.grid(row=1, column=0, columnspan=2, pady=5, padx=10)
 
-t = turtle.RawTurtle(Canvas)
+turt = turtle.RawTurtle(Canvas)
 
-Rotation = random.randint(-350, 350)
-Repeat = random.randint(15, 30)
-Movement = random.randint(5, 40)
+Max_Rotation = 350 #Edit Maximum Rotation Degree Here
+Min_Rotation = -350 #Edit Minimum Rotatio Degree Here
+Max_Repeat = 30 #Edit Maximum Times Here
+Min_Repeat = 15 #Edit Minimum Times Here
+Max_Movement = 20 #Edit Maximum Movement Step Here
+Min_Movement = 5 #Edit Minimum Movement Step Here
+
+Rotation = random.randint(Min_Rotation, Max_Rotation)
+Repeat = random.randint(Min_Repeat, Max_Repeat)
+Movement = random.randint(Min_Movement, Max_Movement)
 
 def Runs():
-    t.pensize(5)
+    turt.pensize(5)
     Run = tkinter.Button(root, text="Run", font=Paragraph, width=10, command=Runs , state=DISABLED)
     Run.grid(row=2, column=0, pady=5)
     Reset = tkinter.Button(root, text="Reset", font=Paragraph, width=10, command=Resets, state=DISABLED)
@@ -33,13 +44,13 @@ def Runs():
         Algorithm = random.randint(1, 4)
 
         if (Algorithm == 1):
-            t.forward(Movement)
+            turt.forward(Movement)
         elif (Algorithm == 2):
-            t.left(Rotation)
+            turt.left(Rotation)
         elif (Algorithm == 3):
-            t.right(Rotation)
+            turt.right(Rotation)
         else:
-            t.backward(Movement)
+            turt.backward(Movement)
     
     Reset = tkinter.Button(root, text="Reset", font=Paragraph, width=10, command=Resets, state=NORMAL)
     Reset.grid(row=2, column=1, pady=5)
@@ -50,13 +61,13 @@ def Runs():
 def Resets():
     Reset = tkinter.Button(root, text="Reset", font=Paragraph, width=10, command=Resets, state=DISABLED)
     Reset.grid(row=2, column=1, pady=5)
-    t.reset()
+    turt.reset()
     Run = tkinter.Button(root, text="Run", font=Paragraph, width=10, command=Runs , state=NORMAL)
     Run.grid(row=2, column=0, pady=5)
     Reset = tkinter.Button(root, text="Reset", font=Paragraph, width=10, command=Resets, state=NORMAL)
     Reset.grid(row=2, column=1, pady=5)
 
-Title = tkinter.Label(root, text="Automatic Random Generated Drawing" ,font=Header)
+Title = tkinter.Label(root, text=Title_Text ,font=Header)
 Title.grid(row=0, column=0, columnspan=2, pady=5)
 Run = tkinter.Button(root, text="Run", font=Paragraph, width=10, command=Runs)
 Run.grid(row=2, column=0, pady=5)
